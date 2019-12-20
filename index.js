444edc2386517765a1af668010fb516127fcedcf
Goodness Ezeokafor <gootech442@yahoo.com>
2019-12-20 05:34:35 +0100

    Fixed Add Functionality

--- index.js
+++ index.js
@@ -188,7 +188,14 @@ $("#addButton").click(async function(){
   var new_deadline = new Date(deadline).getTime()
 
   // console.log(new Date(new_deadline))
-  const new_event = await contractCall('add_project', [title, description, new_deadline,parseInt(goal,10)],0);
+  const new_project = await contractCall('add_project', [title, description, new_deadline,parseInt(goal,10)],0);
+console.log("##########START########")
+console.log("New Project:", new_project)
+console.log("New Project Title:",new_project.title)
+console.log("New Project Description:",new_project.description)
+console.log("New Project Deadline:",new_project.deadline)
+console.log("New Project Goal:",new_project.amountGoal)
+console.log("#########END###########")
 
   // // clear
   $("#title").val("");
